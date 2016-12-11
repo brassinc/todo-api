1d06766ca3bdcea95e9536a4be3f89d64f9caac1
Michael Brassington <brasstrash@gmail.com>
2016-12-11 12:37:36 +0000

    added GET todo by id

--- server.js
+++ server.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT|| 3000;
+var PORT = process.env.PORT || 3000
+var todos =[{
+	id: 1,
+	description: 'Meet mom for lunch',
+	completed: false
+}, {
+	id: 2,
+	description: 'pick up produce',
+	completed: false
+}, {
+	id: 3,
+	description: 'walk the dog',
+	completed: true
+}];
 
 app.get('/', function(req, res) {
 	res.send('Todo API Root');
+});
+
+// GET /todos
+app.get('/todos', function (req, res) {
+	res.json(todos); // converts todos array to json
+})
+
+// GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+	var todoID = parseInt(req.params.id, 10);
+	var matchedTodo;
+
+	
+	todos.forEach(function (todo) {
+		if (todoID === todo.id) {
+			matchedTodo = todo;
+		}
+	});
+
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
 });
 
 app.listen(PORT, function() {
