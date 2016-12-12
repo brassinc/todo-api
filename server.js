f24a665f255e5589c9e15e6bfb844e8898ba52cb
Michael Brassington <brasstrash@gmail.com>
2016-12-12 11:36:19 +0000

    final static todos

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var todos = [];
 
 app.use(bodyParser.json());
 
-// GET /todos?complete=true
+
 app.get('/todos', function (req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -17,6 +17,12 @@ app.get('/todos', function (req, res) {
 		filteredTodos = _.where(filteredTodos, {completed: true});
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
 		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function(todo) {
+			return todo.description.indexOf(queryParams.q) > -1;
+		});
 	}
 	res.json(filteredTodos); // converts todos array to json and sends response
 })
